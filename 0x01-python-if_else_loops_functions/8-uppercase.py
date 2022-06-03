e3e04be23ea35f8eb22d4ae53f4474ac77c8fbdb
Jacques Asinyo <asinyojacques17@gmail.com>
2022-06-03 01:36:44 +0000

    all

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            new_str += chr(ord(str[i]) - 32)
+            continue
+        new_str += str[i]
+
+    print('{0}'.format(new_str))
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+
